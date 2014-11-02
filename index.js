1c8e897316e6615b5a57f7e9a680346fcb101a5e
Brian Tomlinson <darthlukan@gmail.com>
2014-11-02 13:12:51 +0100

    first stab at configurable css

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ var R = React.DOM;
 
 var CardHeader = React.createClass({
     render: function() {
-        return R.div({className: 'card-header'}, [
-            R.img({id: 'user-img', src: this.props.userimg}),
-            R.h3({id: 'username-title'}, this.props.username)
+        return R.div({className: 'card-header', style: this.props.style}, [
+            R.img({id: 'user-img', src: this.props.userimg, style: {
+                borderRadius: '50%',
+                WebkitBorderRadius: '50%',
+                MozBorderRadius: '50%',
+                width: '68px',
+                height: '68px',
+                float: 'left',
+                padding: '0% 4% 0% 4%'
+            }}),
+            R.h3({id: 'username-title', style: {
+                position: 'relative',
+                padding: '1%'
+            }}, this.props.username)
         ]);
     }
 });
@@ -15,17 +26,48 @@ var CardHeader = React.createClass({
 var CardContent = React.createClass({
     render: function() {
         return R.div(
-            {className: 'card-content'}, 
-            R.p({id: 'card-text'}, this.props.text)
+            {className: 'card-content', style: this.props.style},
+            R.p({id: 'card-text', style: {
+                padding: '1%',
+                overflow: 'hidden'
+            }}, this.props.text)
         );
     }
 });
 
 var Card = React.createClass({
     render: function() {
+        if (!this.props.cardStyle) {
+            this.props.cardStyle = {
+                backgroundColor: '#fafafa',
+                width: '30%',
+                height: '20%',
+                overflow: 'hidden',
+                boxShadow: '.25px .25px 5px .25px',
+                borderRadius: '2px 2px 2px 2px',
+                zIndex: 5
+            }
+        }
+
+        if (!this.props.headerStyle) {
+            this.props.headerStyle = {
+                backgroundColor: '#607d8b',
+                padding: '1%',
+                color: '#eceff1',
+                position: 'relative'
+            }
+        }
+
+        if (!this.props.contentStyle) {
+            this.props.contentStyle = {
+                color: 'rgb(0, 0, 0, 0.54)',
+                padding: '2%'
+            }
+        }
+
         return R.div({className: 'card'}, [
-            CardHeader({userimg: this.props.userimg, username: this.props.username}),
-            CardContent({text: this.props.content})
+            CardHeader({userimg: this.props.userimg, username: this.props.username, style: this.props.headerStyle}),
+            CardContent({text: this.props.content, style: this.props.contentStyle})
         ]);
     }
 });
